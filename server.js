2f06d4bbcd1b6e043ee5bb880584eb905bb7f4cb
creightons <creightons26@gmail.com>
2017-06-13 23:55:35 -0400

    - Added feed display to main page

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: './mydb.sqlite3',
+    },
+});
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(morgan());
 
 app.get('/', (req, res) => {
-    res.status(200).render('index');
+    knex.select('name', 'url').from('feedlist')
+        .then(rows => {
+            return res.status(200).render('index', { feeds: rows });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Error');
+        });
 });
 
 app.listen(process.env.PORT || 3000, () => {
